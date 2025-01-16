389342caba2da7471453f7502ce923624413faab
Stefano Suduhakuruge <stefano.sanka@gmail.com>
2025-01-16 07:55:46 +0000

    solved 515.

--- trees-and-graphs/breadth-first-search.ts
+++ trees-and-graphs/breadth-first-search.ts
@@ -32,5 +32,44 @@ var rightSideView = function(root: TreeNode) {
 
     return ret;
 };
+// #endregion
 
+// #region 515. Find largest value in each tree row 
+var largestValues = function(root: any) {
+    if (!root) {
+        return [];
+    }
+
+    let lvl = 0;
+    let queue = [[root, lvl]];
+    let hm = new Map();
+
+    while (queue.length) {
+        const queueLength = queue.length;
+        let q = [];
+
+        for (let i=0;i<queueLength;i++){
+            const node = queue[i][0];
+            const lvl = queue[i][1];
+            
+            hm.set(lvl, Math.max(hm.has(lvl) ? hm.get(lvl) : -Infinity, node.val));
+            
+            if (node.left) {
+                q.push([node.left, lvl+1]);
+            };
+            if (node.right) {
+                q.push([node.right, lvl+1]);
+            }
+        }
+
+        queue = q;
+    }
+
+    let ret = [];
+    for (const [key,val] of hm) {
+        ret.push(val);
+    }
+
+    return ret;
+};
 // #endregion
