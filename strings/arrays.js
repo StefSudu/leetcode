0b4e9dff5d2a2c91c68f06371e8d1f8820f49de9
Stefano Suduhakuruge <stefano.sanka@gmail.com>
2025-02-13 19:35:01 +0000

    needed help with condition part but got most on my own

--- strings/arrays.js
+++ strings/arrays.js
@@ -17,3 +17,23 @@ var kidsWithCandies = function(candies, extraCandies) {
     return arr;
 };
 // #endregion
+
+// #region 605. Can Place Flower
+var canPlaceFlowers = function(flowerbed, n) {
+    if (n === 0) return true;
+
+    for (let i = 0; i < flowerbed.length; i++) {
+        if (
+            flowerbed[i] === 0 && 
+            (i === 0 || flowerbed[i - 1] === 0) && 
+            (i === flowerbed.length - 1 || flowerbed[i + 1] === 0)
+        ) {
+            flowerbed[i] = 1;
+            n--;
+            if (n === 0) return true;
+        }
+    }
+
+    return n === 0;
+};
+// #endregion
