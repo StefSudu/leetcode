0940ff7fa76e087b3fffb5a87473ac36dd3fba61
Stefano Suduhakuruge <stefano.sanka@gmail.com>
2025-02-15 20:15:02 +0000

    time limit exceeded so needed help, enjoyed trying to solve and was almost there

--- strings/arrays.js
+++ strings/arrays.js
@@ -111,3 +111,22 @@ var productExceptSelf = function(nums) {
     return arr;
 };
 // #endregion
+
+// #region 334. Increasing Triplet Sequence
+var increasingTriplet = function(nums) {
+    let first = Infinity;
+    let second = Infinity;
+
+    for (let num of nums) {
+        if (num <= first) {
+            first = num;
+        } else if (num <= second) {
+            second = num;
+        } else {
+            return true;
+        }
+    }
+
+    return false;
+};
+// #endregion
